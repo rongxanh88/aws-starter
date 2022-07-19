972d6f2340b03be27d8863f3dc1131e48f5466f5
Bao Nguyen <rongxanh88@gmail.com>
2022-07-19 16:17:13 -0600

    fix errors

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -46,6 +46,6 @@ aws cloudformation deploy \
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --profile awsbootstrap \
+    --profile $CLI_PROFILE  \
     --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
